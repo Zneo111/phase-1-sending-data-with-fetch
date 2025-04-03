4dac6c1fe77417c2fc97154350e0f74af016be44
Zneo111 <tevin.rutere@student.moringaschool.com>
2025-04-03 10:35:27 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+   
+    const formData = { name, email };
+
+
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => {
+
+        const newIdElement = document.createElement("p");
+        newIdElement.textContent = data.id;
+        document.body.appendChild(newIdElement);
+        return data;
+      })
+      .catch(error => {
+
+        const errorElement = document.createElement("p");
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+      });
+  }
